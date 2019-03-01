1d43fe8b053e639913272ff5e9dc119cb2605b8d
Chip Cerio <alistair.cerio@gmail.com>
2019-03-01 16:50:57 +0800

    added list of random users in HomeScreen

--- src/components/screens/HomeScreen/HomeComponent.js
+++ src/components/screens/HomeScreen/HomeComponent.js
@@ -1,18 +1,25 @@
 import React, { Component } from "react";
 import { Text, View, FlatList } from "react-native";
 import Header from "./../../common/Header";
+import { ListItem } from "react-native-elements";
 
-const HomeComponent = props => (
-  <View>
-    <Header title="Header" />
-    <FlatList
-      data={props.data}
-      keyExtractor={item => item.login.uuid}
-      renderItem={({ item }) => (
-        <Text>{`${item.name.first} ${item.name.last}`}</Text>
-      )}
-    />
-  </View>
-);
+const HomeComponent = props => {
+  return (
+    <View>
+      <Header title="Header" />
+      <FlatList
+        data={props.data}
+        keyExtractor={item => item.login.uuid}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.name.last}
+            subtitle={item.name.first}
+            leftAvatar={{ source: { uri: item.picture.thumbnail } }}
+          />
+        )}
+      />
+    </View>
+  );
+};
 
 export default HomeComponent;
