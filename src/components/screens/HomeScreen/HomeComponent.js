bea9de9bb72a274137c03ee12f198cee9a7d273c
Chip Cerio <alistair.cerio@gmail.com>
2019-03-01 15:39:00 +0800

    transfering FlatList to HomeComponent

--- src/components/screens/HomeScreen/HomeComponent.js
+++ src/components/screens/HomeScreen/HomeComponent.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react'
-import { Text, View ,FlatList} from 'react-native'
-import Header from './../../common/Header'
+import React, { Component } from "react";
+import { Text, View, FlatList } from "react-native";
+import Header from "./../../common/Header";
 
-const HomeComponent = () => {
-  return (
-    <View>
-      <Header 
-      title="Header"/>
+const HomeComponent = props => (
+  <View>
+    <Header title="Header" />
+    <FlatList
+      data={props.data}
+      keyExtractor={item => item.login.uuid}
+      renderItem={({ item }) => (
+        <Text>{`${item.name.first} ${item.name.last}`}</Text>
+      )}
+    />
+  </View>
+);
 
-
-    </View>
-  )
-}
-
-
-export default HomeComponent
+export default HomeComponent;
--- src/components/screens/HomeScreen/HomeContainer.js
+++ src/components/screens/HomeScreen/HomeContainer.js
@@ -1,38 +1,27 @@
-import React, { Component } from 'react'
-import { Text, View, FlatList } from 'react-native'
-import {List, ListItem} from 'react-native-elements'
-import HomeComponent from './HomeComponent'
+import React, { Component } from "react";
+import { Text, View, FlatList } from "react-native";
+import { List, ListItem } from "react-native-elements";
+import HomeComponent from "./HomeComponent";
 
 export class HomeContainer extends Component {
   state = {
     data: []
-  }
+  };
 
-  componentWillMount() {
+  componentDidMount() {
     this.fetchData();
   }
 
-  fetchData = async() => {
+  fetchData = async () => {
     const response = await fetch("https://randomuser.me/api/?results=100");
     const json = await response.json();
-    this.setState({data: json.results});
-  }
+    this.setState({ data: json.results });
+  };
 
   render() {
-    return (
-      <View>
-        {/* <HomeComponent /> */}
-        <FlatList 
-          data={this.state.data}
-          keyExtractor={(x, i) => i}
-          renderItem={({ item }) =>
-            <Text>{`${item.name.first} ${item.name.last}`}</Text>
-            // <Text>{`${item.location.city}`}</Text>
-          }
-        />
-      </View>
-    )
+    console.log(this.state.data);
+    return <HomeComponent data={this.state.data} />;
   }
 }
 
-export default HomeContainer
+export default HomeContainer;
