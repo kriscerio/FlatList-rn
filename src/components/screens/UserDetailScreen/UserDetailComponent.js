21293732341cf0978c62867b2568423c0ac705c3
Chip Cerio <alistair.cerio@gmail.com>
2019-03-04 18:43:53 +0800

    lol

--- src/components/screens/UserDetailScreen/UserDetailComponent.js
+++ src/components/screens/UserDetailScreen/UserDetailComponent.js
@@ -1,15 +1,15 @@
 import React, { Component } from "react";
 import { Text, View, Image } from "react-native";
 import UserDetailConTainer from "./UserDetailContainer";
+import styles from "./styles";
 
 const UserDetailComponent = props => {
   return (
     <View>
-      <Text>{props.lastname}</Text>
-      <Image
-        style={{ height: 100, width: 100, borderRadius: 50 }}
-        source={{ uri: props.thumbnail }}
-      />
+      <View style={styles.containerStyle}>
+        <Image style={styles.avatar} source={{ uri: props.thumbnail }} />
+        <Text style={styles.nameTextStyle}>{`${props.firstname} ${props.lastname}`}</Text>
+      </View>
     </View>
   );
 };
---
+++ src/components/screens/UserDetailScreen/styles.js
@@ -0,0 +1,24 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    marginBottom: 10,
+    alignSelf: "center",
+    position: "absolute",
+    marginTop: 130
+  },
+  containerStyle: {
+    backgroundColor: "#F5F5F5",
+    flex: 1
+  },
+  nameTextStyle: {
+    fontSize: 20
+  }
+});
+
+export default styles;
